a5c532c7fae2d44ed2fa71570b68c4e69207e4c2
FurkanAlan <ulkirr@gmail.com>
2020-09-06 19:39:25 -0500

    Initial commit

---
+++ src/main/java/Sample1.java
@@ -0,0 +1,52 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Sample1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://letskodeit.teachable.com/p/practice");
+
+        WebElement webElement = driver.findElement(By.id("bmwradio"));
+        webElement.click();
+
+        WebElement drpDown = driver.findElement(By.id("carselect"));
+        Select sel = new Select(drpDown);
+        sel.selectByValue("honda");
+
+        WebElement multiSel = driver.findElement(By.id("multiple-select-example"));
+        Select sel2 = new Select(multiSel);
+        sel2.selectByValue("apple");
+        sel2.selectByValue("peach");
+
+        WebElement benzCheck = driver.findElement(By.id("benzcheck"));
+        benzCheck.click();
+
+        WebElement enterNm = driver.findElement(By.name("enter-name"));
+        enterNm.sendKeys("alan");
+
+        WebElement alertBtn = driver.findElement(By.id("alertbtn"));
+        alertBtn.click();
+
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println(alertText);
+        alert.accept();
+
+//        if (alertText=){}
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
